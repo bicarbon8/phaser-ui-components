ba43bb8320c034fba990cfeacbaa5bc79b0502ca
Jason Holt Smith <jason.smith@verizonconnect.com>
2022-11-17 08:10:28 +0000

    update coverage requirements

--- karma.conf.js
+++ karma.conf.js
@@ -40,10 +40,10 @@ module.exports = function (config) {
         check: {
           emitWarning: false,
           global: {
-            statements: 92,
-            branches: 81,
-            functions: 78,
-            lines: 95
+            statements: 90,
+            branches: 80,
+            functions: 80,
+            lines: 90
           }
         }
       },
